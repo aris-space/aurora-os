eebf4c5d5accf5af53f838e5d3bd9426574699f1
Janko Uehlinger <janko.uehlinger@aris-space.ch>
2023-08-28 21:32:19 +0200

    Fix the script for fetching the st distribution package

--- scripts/fetch_distribution_package.sh
+++ scripts/fetch_distribution_package.sh
@@ -1,2 +1,6 @@
+
+cd data/
+mkdir openstlinux-5.10-dunfell-mp1-21-03-31
+cd openstlinux-5.10-dunfell-mp1-21-03-31
 repo init -u https://github.com/STMicroelectronics/oe-manifest.git -b refs/tags/openstlinux-5.10-dunfell-mp1-21-03-31
 repo sync
